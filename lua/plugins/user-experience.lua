cf67786464680a7e8780c04992fb17db3bd07212
Albert <albertwilliams.xyz@gmail.com>
2025-05-25 07:33:14 -0400

    Adding TreeSJ to my list of plugins

--- lua/plugins/user-experience.lua
+++ lua/plugins/user-experience.lua
@@ -30,4 +30,31 @@ return {
 		event = "VeryLazy",
 		opts = {},
 	},
+	{
+		"Wansmer/treesj",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		opts = {
+			use_default_keymaps = false,
+		},
+		keys = {
+			{
+				"<leader>trt",
+				":TSJToggle<CR>",
+				mode = { "n" },
+				desc = "Toggle split/join in a node in the tree",
+			},
+			{
+				"<leader>trs",
+				":TSJSplit<CR>",
+				mode = { "n" },
+				desc = "Toggle split in a node in the tree",
+			},
+			{
+				"<leader>trj",
+				":TSJJoin<CR>",
+				mode = { "n" },
+				desc = "Toggle join in a node in the tree",
+			},
+		},
+	},
 }
