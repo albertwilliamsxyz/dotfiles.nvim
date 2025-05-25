4481ff2f37db4e9fb70a5cb9a4096e50e53c276f
Albert <albertwilliams.xyz@gmail.com>
2025-05-25 01:41:50 -0400

    Improving key mappings and adding new plugin to guide on key mappings

--- lua/plugins/user-experience.lua
+++ lua/plugins/user-experience.lua
@@ -1,30 +1,33 @@
 return {
-        {
-                "ggandor/leap.nvim",
-                dependencies = {
-                        "tpope/vim-repeat",
-                },
-                opts = {
-                        case_sensitive = false,
-                },
-                keys = {
-                        { "<leader>s",  "<Plug>(leap-forward)",     mode = { "n", "x", "o" } },
-                        { "<leader>S",  "<Plug>(leap-backward)",    mode = { "n", "x", "o" } },
-                        { "<leader>gs", "<Plug>(leap-from-window)", mode = { "n", "x", "o" } },
-                },
-        },
-        {
-                "altermo/ultimate-autopair.nvim",
-                event = { "InsertEnter", "CmdlineEnter" },
-                branch = "v0.6",
-                opts = {},
-        },
-        {
-                "kylechui/nvim-surround",
-                version = "*",
-                event = "VeryLazy",
-                config = function()
-                        require("nvim-surround").setup({})
-                end,
-        },
+	{
+		"ggandor/leap.nvim",
+		dependencies = {
+			"tpope/vim-repeat",
+		},
+		opts = {
+			case_sensitive = false,
+		},
+		keys = {
+			{ "<leader>s", "<Plug>(leap-forward)", mode = { "n", "x", "o" }, desc = "Leap forward" },
+			{ "<leader>S", "<Plug>(leap-backward)", mode = { "n", "x", "o" }, desc = "Leap backward" },
+			{
+				"<leader>gs",
+				"<Plug>(leap-from-window)",
+				mode = { "n", "x", "o" },
+				desc = "Leap forward (from window)",
+			},
+		},
+	},
+	{
+		"altermo/ultimate-autopair.nvim",
+		event = { "InsertEnter", "CmdlineEnter" },
+		branch = "v0.6",
+		opts = {},
+	},
+	{
+		"kylechui/nvim-surround",
+		version = "*",
+		event = "VeryLazy",
+		opts = {},
+	},
 }
